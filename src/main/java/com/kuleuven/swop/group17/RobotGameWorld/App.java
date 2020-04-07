b9ff00c4688a9e2b75bcbc040075f2fc5a7e3a4e
Arne Libbrecht <arne.libbrecht@icloud.com>
2020-04-07 13:30:52 +0200

    initial commit

---
+++ src/main/java/com/kuleuven/swop/group17/RobotGameWorld/App.java
@@ -0,0 +1,13 @@
+package com.kuleuven.swop.group17.RobotGameWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
