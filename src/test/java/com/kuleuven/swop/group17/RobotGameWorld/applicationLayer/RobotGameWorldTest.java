28c6b073dffb61e9f5d5c3bf0dd10e73d35338fb
Arne Libbrecht <arne.libbrecht@icloud.com>
2020-04-09 15:28:04 +0200

    start testing

--- src/test/java/com/kuleuven/swop/group17/RobotGameWorld/applicationLayer/RobotGameWorldTest.java
+++ src/test/java/com/kuleuven/swop/group17/RobotGameWorld/applicationLayer/RobotGameWorldTest.java
@@ -3,11 +3,22 @@
  */
 package com.kuleuven.swop.group17.RobotGameWorld.applicationLayer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Field;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.kuleuven.swop.group17.RobotGameWorld.guiLayer.RobotCanvas;
 
 /**
  * RobotGameWorldTest
@@ -15,7 +26,20 @@ import org.junit.Test;
  * @version 0.1
  * @author group17
  */
+@RunWith(MockitoJUnitRunner.class)
 public class RobotGameWorldTest {
+
+	@Rule
+	public ExpectedException exceptionRule = ExpectedException.none();
+
+	@Mock
+	private RobotController robotController;
+
+	@Mock
+	private ElementController elementController;
+
+	@Mock
+	private RobotCanvas robotCanvas;
 
 	/**
 	 * @throws java.lang.Exception
@@ -31,16 +55,57 @@ public class RobotGameWorldTest {
 	public void tearDown() throws Exception {
 	}
 
+	@InjectMocks
+	private RobotGameWorld world;
+
 	/**
-	 * Test method for {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#RobotGameWorld()}.
+	 * Test method for
+	 * {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#RobotGameWorld()}.
 	 */
 	@Test
 	public void testRobotGameWorld() {
-		fail("Not yet implemented");
+		// The constructor of the RobotGameWorld is responsible for creating the
+		// RobotController, the ElementController, the elementRepository and the
+		// RobotCanvas.
+		// It should also add the correct listeners to the needed controllers.
+		// At last The RobotGameWorld itself is also initialized by adding elements and
+		// a robot.
+
+		RobotGameWorld newWorld = new RobotGameWorld();
+		for(Field f : RobotGameWorld.class.getDeclaredFields()) {
+			f.setAccessible(true);
+			try {
+				System.out.println(f.getName()+ " " +f.get(newWorld));
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+			Field f =  RobotGameWorld.class.getDeclaredField("robotController");
+			f.setAccessible(true);
+			assertTrue("RobotController was not initialised", f.get(newWorld) != null);
+			f =  RobotGameWorld.class.getDeclaredField("elementController");
+			f.setAccessible(true);
+			assertTrue("ElementController was not initialised", f.get(newWorld) != null);
+//			f =  RobotGameWorld.class.getDeclaredField("elementRepository");
+//			f.setAccessible(true);
+//			assertTrue("ElementRepository was not initialised", f.get(newWorld) != null);
+			f =  RobotGameWorld.class.getDeclaredField("robotCanvas");
+			f.setAccessible(true);
+			assertTrue("RobotCanvas was not initialised", f.get(newWorld) != null);
+		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+			fail("One or more of the required fields were not declared.");
+		}
+
+//		RobotGameWorld world = new RobotGameWorld();
+//		fail("Not yet implemented");
 	}
 
 	/**
-	 * Test method for {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#performAction(com.kuleuven.swop.group17.GameWorldApi.Action)}.
+	 * Test method for
+	 * {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#performAction(com.kuleuven.swop.group17.GameWorldApi.Action)}.
 	 */
 	@Test
 	public void testPerformAction() {
@@ -48,7 +113,8 @@ public class RobotGameWorldTest {
 	}
 
 	/**
-	 * Test method for {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#evaluate(com.kuleuven.swop.group17.GameWorldApi.Predicate)}.
+	 * Test method for
+	 * {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#evaluate(com.kuleuven.swop.group17.GameWorldApi.Predicate)}.
 	 */
 	@Test
 	public void testEvaluate() {
@@ -56,7 +122,8 @@ public class RobotGameWorldTest {
 	}
 
 	/**
-	 * Test method for {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#saveState()}.
+	 * Test method for
+	 * {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#saveState()}.
 	 */
 	@Test
 	public void testSaveState() {
@@ -64,7 +131,8 @@ public class RobotGameWorldTest {
 	}
 
 	/**
-	 * Test method for {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#restoreState(com.kuleuven.swop.group17.GameWorldApi.GameWorldSnapshot)}.
+	 * Test method for
+	 * {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#restoreState(com.kuleuven.swop.group17.GameWorldApi.GameWorldSnapshot)}.
 	 */
 	@Test
 	public void testRestoreState() {
@@ -72,7 +140,8 @@ public class RobotGameWorldTest {
 	}
 
 	/**
-	 * Test method for {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#paint(java.awt.Graphics)}.
+	 * Test method for
+	 * {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#paint(java.awt.Graphics)}.
 	 */
 	@Test
 	public void testPaint() {
@@ -80,7 +149,8 @@ public class RobotGameWorldTest {
 	}
 
 	/**
-	 * Test method for {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#getType()}.
+	 * Test method for
+	 * {@link com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld#getType()}.
 	 */
 	@Test
 	public void testGetType() {
